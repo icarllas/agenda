93492f08a01a887858c605c5abed79cf91a37ac4
carla <santoscarlla299@gmail.com>
2018-09-13 20:08:44 -0300

    Criando a header da aplicaçao

--- events/models.py
+++ events/models.py
@@ -18,10 +18,17 @@ class Event (models.Model):
     event = models.CharField(max_length=100)
     priority = models.CharField(max_length=1, choices=priorities_list)
 
+    class Meta:
+        ordering = ('-date', '-priority' 'event')
+
+
+    def model_of_comments(self):
+        return self.comment_event.count()
+
     def __str__(self):
         return self.event
 
-class Commit (models.Model):
+class Comment (models.Model):
     """Comentário efetuado em um determinado evento"""
 
     author = models.CharField(max_length=80)
