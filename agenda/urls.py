298fb939cac337a5dfb3e1425e317ad3159fcfa6
carla <santoscarlla299@gmail.com>
2018-10-02 20:40:08 -0300

    fazendo aplicativo blog completo

--- agenda/urls.py
+++ agenda/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('agenda/', include('events.urls')),
+    path('events/', include('events.urls')),
     path('', include('blog.urls')),
 ]
